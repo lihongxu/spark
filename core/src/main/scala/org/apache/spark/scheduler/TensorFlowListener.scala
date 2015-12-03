df6947677f06240d5420425eedb6531bd26a6e12
Timothy Hunter <timhunter@databricks.com>
2015-12-02 20:20:28 -0800

    tensorflow defs

---
+++ core/src/main/scala/org/apache/spark/scheduler/TensorFlowListener.scala
@@ -0,0 +1,11 @@
+package org.apache.spark.scheduler
+
+import org.apache.spark.Logging
+
+/**
+ * Created by tjhunter on 12/2/15.
+ */
+class TensorFlowListener extends SparkListener with Logging {
+
+
+}
