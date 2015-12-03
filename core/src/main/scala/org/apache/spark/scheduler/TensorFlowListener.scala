fd8f3c403f2bffe261c94773625eeceaa09e84fc
Timothy Hunter <timhunter@databricks.com>
2015-12-02 20:35:59 -0800

    changes

--- core/src/main/scala/org/apache/spark/scheduler/TensorFlowListener.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TensorFlowListener.scala
@@ -1,11 +1,19 @@
 package org.apache.spark.scheduler
 
-import org.apache.spark.Logging
+import java.io.{FileOutputStream, DataOutputStream}
 
-/**
- * Created by tjhunter on 12/2/15.
- */
+import org.apache.spark.Logging
+import tensorflow.EventOuterClass
+
 class TensorFlowListener extends SparkListener with Logging {
 
+  val file = "/Users/tjhunter/work/tensorflow_mount/logs/out"
 
+  lazy val stream = new DataOutputStream(new FileOutputStream(file))
+
+  def init(): Unit = {
+    val event = EventOuterClass.Event.newBuilder().setWallTime(1.0).setStep(1L).build()
+    stream.write(event.toByteArray)
+    stream.flush()
+  }
 }
